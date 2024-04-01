ed74ede2b9f8d445175d43c59a2815c6135e2989
refa3211 <111779078+refa3211@users.noreply.github.com>
2024-04-01 12:29:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,33 +17,33 @@ def fetch_hosts_from_github(repo_url):
 def input_hostname_to_nextdns(hostnames):
     driver = webdriver.Chrome()  # Assuming you have Chrome WebDriver installed
     driver.get("https://my.nextdns.io/login")
+    time.sleep(1)
 
-    # Replace 'YOUR_EMAIL' and 'YOUR_PASSWORD' with your NextDNS credentials
-    # email_input = driver.find_element("xpath", "//input[@type='email']")
-    email_input = driver.find_element(By.CLASS_NAME,"form-control")
+    email_input = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/form/div[1]/input")
     email_input.send_keys("refa3211@telegmail.com")
 
-    # password_input = driver.find_element("xpath", "//input[@type='password']")
-    password_input = driver.find_element(By.CLASS_NAME, "form-control")
-    password_input.send_keys("test")
+    password_input = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[2]/div/form/div[2]/input")
+    password_input.send_keys("R3fa3@#ldns")
+
     password_input.send_keys(Keys.RETURN)
 
-    time.sleep(5)  # Wait for login
+    time.sleep(1)  # Wait for login
 
     for hostname in hostnames:
         if hostname.strip():
-            driver.get("https://https://my.nextdns.io/159376/denylist")
-            time.sleep(5)  # Wait for page to load
-            add_domain_input = driver.find_element_by_xpath("//input[@name='Add a domain...']")
+            driver.get("https://my.nextdns.io/159376/denylist")
+            time.sleep(0.5)  # Wait for page to load
+            add_domain_input = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div/div/div[1]/form/div/input")
             add_domain_input.send_keys(hostname)
+            print(hostname)
             add_domain_input.send_keys(Keys.RETURN)
-            time.sleep(2)  # Wait for domain to be added
+            time.sleep(1)  # Wait for domain to be added
 
     driver.quit()
 
 
 if __name__ == "__main__":
     # Replace 'GITHUB_REPO_URL' with the URL of your GitHub repository containing the hosts list
-    github_repo_url = "https://raw.githubusercontent.com/GeorgeForse/VPN-List/master/list.txt"
+    github_repo_url = "https://raw.githubusercontent.com/refa3211/nextedit/main/host"
     hosts = fetch_hosts_from_github(github_repo_url)
     input_hostname_to_nextdns(hosts)
